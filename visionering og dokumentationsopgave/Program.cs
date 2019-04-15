d56a3b692524aa139d57ec0b2b4d9f2f22d95185
Melika <mevp1997@hotmail.com>
2019-04-15 12:53:35 +0200

    Added the project

---
+++ visionering og dokumentationsopgave/Program.cs
@@ -0,0 +1,193 @@
+﻿//VersionControll_Application:
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace visionering_og_dokumentationsopgave
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool loop = true;
+            string tast = "";
+            Score score = new Score();
+            double currentScore = score.points(0, 0);
+            Questions question = new Questions();
+
+            while (loop == true)
+            {
+                Console.WriteLine("Press 1 to play.");
+                Console.WriteLine("Press 2 to exit.");
+                tast = Console.ReadLine();
+
+                if (tast == "1")
+                {   
+                    question.selectQuestions();
+                }
+
+                if (tast == "2")
+                {
+                    loop = false;
+                }
+
+            }
+
+        }
+    }
+    class Questions
+    {
+        bool svar;
+        public void selectQuestions()
+        {
+            int x = 4;
+            Random random = new Random();
+            
+            int randomNumber = random.Next(0, x);
+            string randNum = Convert.ToString(randomNumber);
+
+
+            switch (randNum)
+            {
+                case "0":
+                    svar = questionOne();
+                    break;
+                case "1":
+                    svar = questionTwo();
+                    break;
+                case "2":
+                    svar = questionThree();
+                    break;
+                case "3":
+                    svar = questionFour();
+                    break;
+                case "4":
+                    svar = questionFive();
+                    break;
+
+                default:
+                    break;
+            }
+
+            isCorrect(svar);
+
+
+
+        }
+
+        public void isCorrect(bool a)
+        {
+            if (a == true)
+            {
+                Console.WriteLine("That is correct");
+            }
+            else
+            {
+                Console.WriteLine("That is not correct.");
+            }
+        }
+
+        public bool questionOne()
+        {
+            bool svar = false;
+            string tast = "";
+            Console.WriteLine("What got the largest gravitational pull?");
+            Console.WriteLine("");
+            Console.WriteLine("A - The Sun");
+            Console.WriteLine("B - A black hole");
+            Console.WriteLine("C - Yo mama");
+            tast = Console.ReadLine();
+            
+            if (tast.ToLower() == "b")
+            {
+                svar = true;
+            }
+
+            return svar;
+        }
+
+        public bool questionTwo()
+        {
+            bool svar = false;
+            string tast = "";
+            Console.WriteLine ("Which year is it 2 years after, 4 years before?");
+            Console.WriteLine("");
+            Console.WriteLine("A - 2015");
+            Console.WriteLine("B - 2019");
+            Console.WriteLine("C - 2017");
+            tast = Console.ReadLine();
+
+            if (tast.ToLower() == "c")
+            {
+                svar = true;
+            }
+
+            return svar;
+        }
+
+
+        public bool questionThree()
+        {
+            bool svar = false;
+            string tast = "";
+            Console.WriteLine("Who is the president of the United States?");
+            Console.WriteLine("");
+            Console.WriteLine("A - Donald Trump");
+            Console.WriteLine("B - Barack Obama");
+            Console.WriteLine("C - Hillary Clinton");
+            tast = Console.ReadLine();
+
+            if (tast.ToLower() == "a")
+            {
+                svar = true;
+            }
+
+            return svar;
+        }
+
+        public bool questionFour()
+        {
+            bool svar = false;
+            string tast = "";
+            Console.WriteLine("Do Multiverses exist?");
+            Console.WriteLine("");
+            Console.WriteLine("A - Maybe");
+            Console.WriteLine("B - Yes");
+            Console.WriteLine("C - Only in fiction");
+            tast = Console.ReadLine();
+
+            svar = true;
+            return svar;
+        }
+
+        public bool questionFive()
+        {
+            bool svar = false;
+            string tast = "";
+            Console.WriteLine("Which Traditional beloved Disney movie?");
+            Console.WriteLine("");
+            Console.WriteLine("A - The Jungle Book");
+            Console.WriteLine("B - Beauty and the Beast");
+            Console.WriteLine("C - The Lion King");
+            tast = Console.ReadLine();
+
+            if (tast.ToLower() == "c")
+            {
+                svar = true;
+            }
+
+            return svar;
+        }
+
+    }
+    class Score
+    { 
+        public double points(double a, double b)
+        {
+            double point = a + b;
+            return point;
+        }
+    }
+}
