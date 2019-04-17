17010e437a34e3ec868145ff3092cf8778343f5d
Sebastian <im.cool@live.dk>
2019-04-17 08:19:20 +0200

    Highscores

--- visionering og dokumentationsopgave/Score.cs
+++ visionering og dokumentationsopgave/Score.cs
@@ -28,6 +28,35 @@ namespace visionering_og_dokumentationsopgave
             return new_score;
         }
 
+        public void Highscore(double score)
+        {
+            string[] prev_scores = System.IO.File.ReadAllLines(@"Scores.txt");
+            string[] all_scores = new string[prev_scores.Length + 1];
+            double highest_score = 0;
+            int i = 0;
+
+            while (i < prev_scores.Length)
+            {
+                all_scores[i] = prev_scores[i];
+
+                if (Convert.ToInt32(all_scores[i]) > Convert.ToInt32(highest_score))
+                {
+                    highest_score = Convert.ToInt32(all_scores[i]);
+                }
+                i++;
+            }
+
+            all_scores[i] = score.ToString();
+
+            if (score > highest_score)
+            {
+                highest_score = score;
+                Console.WriteLine("Congrats, you have the newest highscore!!!");
+            }
+
+            System.IO.File.WriteAllLines(@"Scores.txt", all_scores);
+        }
+
 
     }
 }
